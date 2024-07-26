5e768a7f5d1bb2ec5e32fe90bf87ef941dde6071
Chandru TS <chandruts25@outlook.com>
2024-07-26 16:21:19 +0530

    Finalyzed

--- ApryseConsolePdf_to_Excel/Program.cs
+++ ApryseConsolePdf_to_Excel/Program.cs
@@ -19,7 +19,6 @@ namespace PDF2OfficeTestCS
         const string inputPath = @"C:\Users\hp\Downloads\";
         const string outputPath = inputPath;
 
-        //[STAThread]
         static int Main(string[] args)
         {
             PDFNet.Initialize("demo:1721917547091:7e6e33d60300000000a1be6388807171fb0a9afe315df0f72c6f73122d");
@@ -37,52 +36,51 @@ namespace PDF2OfficeTestCS
             // Excel
             List<string> fileNames = new List<string>
             {
-                "2024_07_01_-Wyoming-Medicaid-Exclusion-List-July",
+                //"2024_07_01_-Wyoming-Medicaid-Exclusion-List-July",
                 "AlaskaExcludedProviderList",
-                "Idaho Medicaid Exclusion List",
-                "Med Prov Excl-Rein List-UPDATED-07.10.2024",
-                "Medicaid Excluded Providers",
-                "nj_debarment_list (1)",
-                "provider-exclusion-list",
-                "ProviderSuspensionsTerminations",
+                //"Idaho Medicaid Exclusion List",
+                //"Med Prov Excl-Rein List-UPDATED-07.10.2024",
+                //"Medicaid Excluded Providers",
+                //"nj_debarment_list (1)",
+                //"provider-exclusion-list",
+                //"ProviderSuspensionsTerminations",
                 "terminatedproviderlist",
                 "WV Medicaid Provider Exclusions and Terminations July 2024"
             };
 
-            //foreach (var fileName in fileNames)
-            //{
-            try
+            foreach (var fileName in fileNames)
             {
-                //var fileName = "terminatedproviderlist";
-                var fileName = "6546565464";
+                try
+                {
+                    //var fileName = "terminatedproviderlist";
+                    //var fileName = "6546565464";
+                    //var fileName = "WV Medicaid Provider Exclusions and Terminations July 2024";
 
-                string outputFile = outputPath + fileName + ".xlsx";
+                    string outputFile = outputPath + fileName + ".xlsx";
 
-                pdftron.PDF.Convert.ExcelOutputOptions options = new pdftron.PDF.Convert.ExcelOutputOptions();
+                    pdftron.PDF.Convert.ExcelOutputOptions options = new pdftron.PDF.Convert.ExcelOutputOptions();
 
-                options.SetNonTableContent(true);
-                // For Add header for all tables
-                options.SetSingleSheet(true);
-                // For multiple sheets
-                options.SetPageSingleSheet(true);
-                options.SetHeadersAndFootersSetting(pdftron.PDF.Convert.StructuredOutputOptions.SectionConversionSetting.e_Recover);
+                    options.SetNonTableContent(true);
+                    options.SetSingleSheet(true);
+                    options.SetPageSingleSheet(true);
+                    options.SetHeadersAndFootersSetting(pdftron.PDF.Convert.StructuredOutputOptions.SectionConversionSetting.e_Recover);
 
-                pdftron.PDF.Convert.ToExcel(inputPath + fileName + ".pdf", outputFile, options);
+                    pdftron.PDF.Convert.ToExcel(inputPath + fileName + ".pdf", outputFile, options);
 
-                Console.WriteLine("Result saved in " + outputFile);
+                    Console.WriteLine("Result saved in " + outputFile);
+                }
+                catch (PDFNetException e)
+                {
+                    Console.WriteLine("Unable to convert PDF document to Excel, error: " + e.Message);
+                    err = true;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unknown Exception, error: ");
+                    Console.WriteLine(e);
+                    err = true;
+                }
             }
-            catch (PDFNetException e)
-            {
-                Console.WriteLine("Unable to convert PDF document to Excel, error: " + e.Message);
-                err = true;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Unknown Exception, error: ");
-                Console.WriteLine(e);
-                err = true;
-            }
-            //}
 
             PDFNet.Terminate();
             Console.WriteLine("Done.");
